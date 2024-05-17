7ab8fbcd3a620b9751a1c7c4f4c1371b3715b09f
Jorge Luiz da Silva <jlsilva01@yahoo.com.br>
2024-05-17 14:17:26 -0300

    docs: atualizacao na documentao do projeto

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -1,8 +1,6 @@
 """Módulo para transformação de dados."""
 from typing import List
-
 import pandas as pd
-
 
 def contact_data_frames(dataframe_list: List[pd.DataFrame]) -> pd.DataFrame:
     """Função para transformar uma lista de dataframes em um único dataframe.
